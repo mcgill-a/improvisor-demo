ff16e7ebd8dd5a33b34c35efd2aafb2e82cacaef
Alex <alex.mcgill@outlook.com>
2019-02-03 22:36:41 +0000

    Initial commit

---
+++ run.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecret'
+
+bootstrap = Bootstrap(app)
+socketio = SocketIO(app)
+
+@socketio.on('message')
+def handleMessage(msg):
+    print('Message: ' + str(msg))
+    send(msg, broadcast=True)
+
+
+@app.route('/', methods=['GET'])
+def index():
+	tags = ['database', 'server', 'improvise', 'group project', 'image', 'youtube video']
+	return render_template('index.html', tags=tags)
+
+@app.route('/presenter', methods=['GET'])
+def presenter():
+	return render_template('presenter.html')
+
+@app.route('/controller', methods=['GET'])
+def controller():
+	tags = ['database', 'server', 'improvise', 'group project', 'image', 'youtube video']
+	return render_template('controller.html', tags=tags)
+
+
+if __name__ == '__main__':
+    print("")
+    print("--- --- --- --- --- --- ---")
+    print("-- Improvisor Web Server --")
+    print("--- --- --- --- --- --- ---")
+    print("- Connected: SocketIO")
+    print("- Connected: MySQL Database")
+    print("--- --- --- --- --- --- ---")
+    print("")
+    socketio.run(app)
